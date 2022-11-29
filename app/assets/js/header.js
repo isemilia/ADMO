c14924bff9f9e3977af19313f782e1f1ef419b7d
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-11-29 20:25:31 +0600

    Make nav menu responsive

---
+++ app/assets/js/header.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+    function openMenu(menuSelector, triggerSelector, menuActiveClass, triggerActiveClass) {
+        const menu = document.querySelector(menuSelector);
+        const trigger = document.querySelector(triggerSelector);
+
+        trigger.addEventListener('click', e => {
+            e.preventDefault();
+            menu.classList.toggle(menuActiveClass);
+            trigger.classList.toggle(triggerActiveClass);
+        });
+
+        window.addEventListener('click', function(e) {
+            console.log(e.target);
+            if (e.target != menu && e.target !=trigger && menu.classList.contains(menuActiveClass)) {
+                menu.classList.remove(menuActiveClass);
+                trigger.classList.remove(triggerActiveClass);
+            }
+        });
+    }
+
+    openMenu('[data-header-nav]', '[data-nav-trigger]', 'header-nav--active', 'header-hamburger--active');
+})

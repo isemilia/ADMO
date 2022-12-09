2b1e89aacb08635d40970e87705c77ae20fdbfb0
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-12-09 11:22:30 +0600

    Add functionality to cities search

---
+++ app/assets/js/partners.js
@@ -0,0 +1,58 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const searchWrap = document.querySelector('[data-search-wrap]');
+    const cities = document.querySelectorAll('[data-city]');
+    const citiesList = document.querySelector('[data-cities-list]');
+    const citiesWrap = document.querySelector('[data-cities-wrap]');
+    const tabs = document.querySelectorAll('[data-tab]');
+
+    const input = searchWrap.querySelector('input.search-input');
+
+    function hideAllTabs() {
+        tabs.forEach(tab => {
+            tab.classList.add('hidden');
+        });
+    }
+
+    function unmarkAllCities() {
+        cities.forEach(city => {
+            city.classList.remove('cities-item--active');
+        });
+    }
+
+    input.addEventListener('click', () => {
+        citiesWrap.classList.add('hero-cities--active');
+        window.scrollTo({
+            behavior: "smooth",
+            top: input.offsetTop - 70
+        });
+        window.addEventListener('click', (e) => {
+            // console.log(e.target);
+            if (e.target != citiesWrap && e.target != citiesList && e.target != input && !e.target.classList.contains('cities-item')) {
+                citiesWrap.classList.remove('hero-cities--active');
+            }
+        });
+    });
+
+    input.addEventListener('input', () => {
+        cities.forEach(city => {
+            if (!input.length) {
+                city.classList.remove('cities-item--active');
+            }
+            if(!city.textContent.toLowerCase().includes(input.value.toLowerCase())) {
+                city.classList.remove('cities-item--active');
+            } else if (input.value.toLowerCase() != '') {
+                city.classList.add('cities-item--active');
+            }
+        })
+    });
+
+    cities.forEach((city, i) => {
+        city.addEventListener('click', () => {
+            hideAllTabs();
+            tabs[i].classList.remove('hidden');
+            input.value = city.textContent;
+            unmarkAllCities()
+            city.classList.add('cities-item--active');
+        });
+    });
+});

3cb1e594d3333f112b2653d532b7e082b154fa51
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-12-13 10:54:19 +0600

    Scroll top plan table on open

--- app/assets/js/plan.js
+++ app/assets/js/plan.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const planBtn = document.querySelector('[data-plan-btn]');
     const planInfo = document.querySelector('[data-plan-info]');
+    const plan = document.querySelector('.plan');
 
     function changeBtnText() {
         if(planBtn.innerText == planBtn.getAttribute('data-plan-btn-open')) {
@@ -16,10 +17,18 @@ window.addEventListener('DOMContentLoaded', () => {
             planInfo.style.maxHeight = planInfo.scrollHeight + 'px';
             planInfo.classList.toggle('plan-info-inner--open');
             changeBtnText();
+            window.scrollTo({
+                top: planInfo.offsetTop,
+                behavior: 'smooth'
+            });
         } else {
             planInfo.style.maxHeight = 0;
             planInfo.classList.toggle('plan-info-inner--open');
             changeBtnText();
+            window.scrollTo({
+                top: plan.offsetTop,
+                behavior: 'smooth'
+            });
         }
     });
 });
